000ba79f6a8be00d5c86cd8a372565f4237989df
Jacob Gayban <eanj13@gmail.com>
2022-03-10 15:02:28 -0800

    Using Cell in app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css'
+import Cell from './components/Cell'
 
 const App = () => {
-    const board = new Array(64).fill(<div className="box">X</div>)
+    const board = new Array(64).fill(<Cell row={0} column={0} value={'X'} />)
     return <div className="game-board">{board}</div>
 }
 
