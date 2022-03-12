aecbee6196a4720a6dce2310f23c4cb088972f4b
Jacob Gayban <eanj13@gmail.com>
2022-03-11 21:35:44 -0800

    Cells can dispatch makeWhite action

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,10 +1,12 @@
 import { useDispatch } from 'react-redux'
+import { makeWhite } from '../reducers/boardReducer'
 
 const Cell = ({ row, column, value }) => {
     const dispatch = useDispatch()
 
     const onClick = () => {
         console.log(row, column)
+        dispatch(makeWhite(row, column))
     }
 
     return (
